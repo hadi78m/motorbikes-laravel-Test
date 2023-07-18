b2082dbdd7e7eaf47314e60763296a2345acc427
hadi78m <hmggmstar@gmail.com>
2023-07-18 13:29:09 +0330

    first commit

---
+++ database/migrations/2023_07_18_093531_create_motors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('motors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('model',50);
+            $table->string('color',30);
+            $table->string('price',50);
+            $table->string('image',100);
+            $table->string('weight',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('motors');
+    }
+};
